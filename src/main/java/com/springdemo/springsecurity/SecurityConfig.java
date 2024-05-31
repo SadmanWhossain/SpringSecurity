99a0ca586ea133ce33827d3cffcb065187a96d90
SadmanWhossain <72847790+SadmanWhossain@users.noreply.github.com>
2024-05-31 17:52:12 +0600

    Database Connection

---
+++ src/main/java/com/springdemo/springsecurity/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.springdemo.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated()).httpBasic(Customizer.withDefaults()).formLogin(Customizer.withDefaults()).build();
+    }
+}
